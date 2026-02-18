d8c9d8aa1b8ded0c648943831accbf1a1ac8866f
fell-z <matheusfelipeagapito@hotmail.com>
2026-02-18 15:44:18 -0300

    Add airport and flight models

---
+++ db/migrate/20260217180156_create_flights.rb
@@ -0,0 +1,12 @@
+class CreateFlights < ActiveRecord::Migration[8.1]
+  def change
+    create_table :flights do |t|
+      t.belongs_to :departure_airport, null: false, foreign_key: { to_table: :airports }
+      t.belongs_to :arrival_airport, null: false, foreign_key: { to_table: :airports }
+      t.datetime :date
+      t.integer :duration
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+[ "LAX", "ATL", "DEN", "JFK", "LAS", "MIA", "SEA", "SFO", "BOS", "PHL", "SAN", "AUS" ].each do |code|
+  Airport.find_or_create_by!(code: code)
+end
+
+14.times do |i|
+  Airport.ids.permutation(2).each do |departure_airport_id, arrival_airport_id|
+    rand(2..6).times do
+      Flight.find_or_create_by!(
+        departure_airport_id: departure_airport_id,
+        arrival_airport_id: arrival_airport_id,
+        date: Time.current.midnight + (i + 1).day + rand(1..24).hour,
+        duration: (1..6).step(0.5).map { |time| time.hour.in_minutes }.sample
+      )
+    end
+  end
+end
