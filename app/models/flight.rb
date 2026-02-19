0466324c1c3e1bbd3b6b15c6f9fa46ab6b9f8291
fell-z <matheusfelipeagapito@hotmail.com>
2026-02-19 13:12:18 -0300

    Add methods to return formatted flight attributes

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,19 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  def self.available_dates
+    # Self reminder: passing a symbol to ::order doesn't work when a string is passed to ::select
+    select("date_trunc('day', date) AS date").distinct.order("date")
+  end
+
+  def pretty_date
+    date.strftime("%d/%m/%Y")
+  end
+
+  def pretty_format
+    ERB::Util.html_escape_once(
+      "#{departure_airport.code} &rArr; #{arrival_airport.code}, departing at #{date.to_fs(:time)}"
+    )
+  end
 end
