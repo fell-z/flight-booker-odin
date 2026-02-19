06899d5c9de2b72369b69ab9fca9363b792e820c
fell-z <matheusfelipeagapito@hotmail.com>
2026-02-19 13:16:09 -0300

    Create flights controller with index action
    - Added search form to query flights with given user input.
    - Made that, if present, flights will show in the same page as search
      form with radio buttons to select one.

---
+++ app/controllers/flights_controller.rb
@@ -0,0 +1,22 @@
+class FlightsController < ApplicationController
+  def index
+    @airport_codes_options = Airport.pluck(:code)
+    @available_dates = Flight.available_dates
+
+    if search_sent?
+      @flights = Flight.where(
+        departure_airport: Airport.find_by(code: params[:departure_code]),
+        arrival_airport: Airport.find_by(code: params[:arrival_code]),
+        date: params[:date].to_date.all_day
+      )
+
+      @number_of_passengers = params[:number_of_passengers]
+    end
+  end
+
+  private
+
+  def search_sent?
+    %i[departure_code arrival_code date number_of_passengers].all? { |necessary_key| params.has_key?(necessary_key) }
+  end
+end
